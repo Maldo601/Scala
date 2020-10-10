0595a0a78b18c9918b97ac653ea9bb27a8fd06a0
Maldo601 <maldo601@gmail.com>
2020-10-10 20:33:45 +0200

    Masters

--- src/main/scala/A_BASES/D_Conversiones.scala
+++ src/main/scala/A_BASES/D_Conversiones.scala
@@ -3,11 +3,14 @@ package A_BASES
 object D_Conversiones extends App {
     
   val real: Float = 100;
-  val doble: Double = real;
-
   val caracter: Int = 'A'
   val unicode: Double = caracter
 
-        println(doble)
-        println(unicode)
+  println(real.toDouble)
+  println(unicode)
+
+
+
+
+        
 }
