1e74329adafc89629de667618bfcc90aa2a491d1
Maldo601 <maldo601@gmail.com>
2020-10-10 20:56:38 +0200

    Masters

--- src/main/scala/A_BASES/E_ConversionesSimplificadas.scala
+++ src/main/scala/A_BASES/E_ConversionesSimplificadas.scala
@@ -1,6 +1,6 @@
 package A_BASES
 
-object E_ConversionNoNumerica extends App {
+object E_ConversionesSimplificadas extends App {
     
     val a = 100L
     val b = a.toDouble // U otros tipos casteables. 
@@ -8,5 +8,9 @@ object E_ConversionNoNumerica extends App {
     val c = 10.57
     val d = c.toInt
 
+    // Simplifiquemos:
+    val e = 10.59.toInt
     println(b,d)
+    println(e)
+
 } 
