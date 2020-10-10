d27cf049fa97b629a75d0abba4840cae7e1c75c2
Maldo601 <maldo601@gmail.com>
2020-10-10 19:41:25 +0200

    Masters

---
+++ src/main/scala/A_BASES/B_AvaluarExpresionesScala.scala
@@ -0,0 +1,11 @@
+package A_BASES
+
+object B_AvaluarExpresionesScala extends App {
+    //Scala tiene tendencia simplificarnos o sugerirnos la simplificacion de los operadores, no obstante,
+    //podemos usarlo o no. Vamos a ver como opera este lenguaje una operación que mezcla boleanos, ints, chars, etc. 
+
+    // Tenemos aqui el mismo ejemplo usado en Java. Opera exactamente igual y solo nos ahorra el System.out
+
+    println(!false && (3>=8.4*5)||(3+7.0<=10.0+(6*5/7+9+67)))
+  
+}
