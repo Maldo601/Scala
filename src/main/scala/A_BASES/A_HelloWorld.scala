b02d66c744e68064fc9ca2a5f647542cf49f9be9
Maldo601 <maldo601@gmail.com>
2020-10-10 19:10:47 +0200

    Masters

---
+++ src/main/scala/A_BASES/A_HelloWorld.scala
@@ -0,0 +1,5 @@
+package A_BASES
+
+object A_HelloWorld extends App {
+  println("\n\t\t"+"Hello World, Scala ! :D " + "\n")
+}
---
+++ src/main/scala/A_BASES/C_DeclararVariables.scala
@@ -0,0 +1,10 @@
+
+package A_BASES
+object C_DeclararVariables extends App {
+  val a:Char = 65
+  val b:Double = 5.2
+  val c:Int = 'M'
+  val d:String = "Hola"
+  println("\n\t"+a+"\n"+"\t\t"+b+"\n"+"\t\t\t"+c+"\n"+"\t\t\t\t"+d+"\n")
+  println("hola")
+}
