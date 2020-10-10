59027196d33e961edf37ffc131836f86e15faa97
Maldo601 <maldo601@gmail.com>
2020-10-10 20:52:14 +0200

    Masters

---
+++ src/main/scala/A_BASES/E_ConversionNoNumerica.scala
@@ -0,0 +1,12 @@
+package A_BASES
+
+object E_ConversionNoNumerica extends App {
+    
+    val a = 100L
+    val b = a.toDouble // U otros tipos casteables. 
+
+    val c = 10.57
+    val d = c.toInt
+
+    println(b,d)
+} 
