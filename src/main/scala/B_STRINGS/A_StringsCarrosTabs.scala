791dc0a6b38f748e7533d0183e9fd2e7b2decdc1
Maldo601 <maldo601@gmail.com>
2020-10-10 20:41:14 +0200

    Masters

---
+++ src/main/scala/B_STRINGS/A_StringsCarrosTabs.scala
@@ -0,0 +1,6 @@
+package B_STRINGS
+
+object A_StringsCarrosTabs extends App {
+  // Es bastante similar, por no decir exacto a Java en este caso:
+  println("\tHola" + "\nAvui fa un bon dia" +"\n\t\tDema segurament també farà un bon dia");
+}
